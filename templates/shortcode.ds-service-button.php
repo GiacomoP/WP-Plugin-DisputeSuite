a6e594e0dc87147f8feaf29e3dcf8c075b695df2
Giacomo Persichini <giacomopersichini@gmail.com>
2016-08-08 01:05:58 +0100

    Add a link to the services buttons to start the first step

--- templates/shortcode.ds-service-button.php
+++ templates/shortcode.ds-service-button.php
@@ -1,17 +1,20 @@
 <?php
 
+use DisputeSuite\PostTypes\Step;
+
 $image = $data->hasCoclient() ? 'ds-couple.png' : 'ds-individual.png';
 $imagePath = "{$template['imagesPath']}/{$image}";
 $class = $data->hasCoclient() ? 'ds-service-couple' : 'ds-service-individual';
+$stepSlug = Step::SLUG;
 
 ?>
 
-<div class="ds-service <?php echo $class; ?>" data-product="<?php echo $data->getId(); ?>">
-    <div class="icon-container">
-        <div class="icon"><img src="<?php echo $imagePath; ?>"></div>
-    </div>
-    <div class="name-container">
+<a class="ds-service <?php echo $class; ?>" data-product="<?php echo $data->getId(); ?>" href="<?php echo home_url("$stepSlug/client-info/?service={$data->getId()}", 'relative'); ?>">
+    <span class="icon-container">
+        <span class="icon"><img src="<?php echo $imagePath; ?>"></span>
+    </span>
+    <span class="name-container">
         <span class="title"><?php echo $data->getLabel(); ?></span>
         <span class="price"><?php echo "\${$data->getSetupFee()} + \${$data->getMonthlyFee()}/mo"; ?></span>
-    </div>
-</div>
+    </span>
+</a>
